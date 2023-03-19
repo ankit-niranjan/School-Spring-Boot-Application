17c089ec4a5a8602e4004f02a01632de30e0b700
ankit-niranjan <ankit.niranjan@thoughtworks.com>
2023-03-19 14:51:37 +0530

    basic thymeleaf functionality

--- src/main/java/com/playground/learn/controller/HomeController.java
+++ src/main/java/com/playground/learn/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.playground.learn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "John Wick");
         return "home.html";
     }
 }
